3dd60deda6767efe24ea9876b1df41ddc1a31895
Vedant Chainani <vedantchainani1084@gmail.com>
2023-09-15 19:32:43 +0530

    add custom wallets

--- src/providers/web3/index.tsx
+++ src/providers/web3/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { ThirdwebProvider } from '@thirdweb-dev/react';
+import {
+	ThirdwebProvider,
+	metamaskWallet,
+	walletConnect,
+	coinbaseWallet,
+	trustWallet,
+	localWallet,
+} from '@thirdweb-dev/react';
 import { Ethereum, Polygon } from '@thirdweb-dev/chains';
 
 // Metadata
 import { dAppMetadata } from 'config';
 
 import { env } from '~/env.mjs';
+
+const { NEXT_PUBLIC_TW_CLIENT_ID, NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID } = env;
 
 interface Props {
 	children: React.ReactNode;
@@ -14,10 +23,21 @@ interface Props {
 const Web3Provider = ({ children }: Props) => {
 	return (
 		<ThirdwebProvider
-			clientId={env.NEXT_PUBLIC_TW_CLIENT_ID}
+			clientId={NEXT_PUBLIC_TW_CLIENT_ID}
 			activeChain={Ethereum}
 			supportedChains={[Ethereum, Polygon]}
 			dAppMeta={dAppMetadata}
+			supportedWallets={[
+				metamaskWallet(),
+				coinbaseWallet(),
+				walletConnect({
+					projectId: NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
+				}),
+				trustWallet({
+					projectId: NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
+				}),
+				localWallet(),
+			]}
 		>
 			{children}
 		</ThirdwebProvider>
