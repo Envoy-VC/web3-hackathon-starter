3e4adf5a042435dff0d902b3cdfca765e1ba49b4
Vedant Chainani <vedantchainani1084@gmail.com>
2023-10-07 01:46:15 +0530

    shift to pnpm as package manager and remove web3 config

--- config/index.ts
+++
@@ -1,7 +0,0 @@
-export const dAppMetadata = {
-	name: 'My dApp',
-	description: 'My dApp description',
-	logoUrl: 'https://my-dapp.com/logo.png',
-	url: 'https://my-dapp.com',
-	isDarkMode: true,
-};
